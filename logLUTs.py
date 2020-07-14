3cbc88350352748b472f60259fc800f801d0a62e
matt venn <matt@mattvenn.net>
2020-07-14 11:40:24 +0200

    reqs and remove import datetime

--- logLUTs.py
+++ logLUTs.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
+import git
 import re
 import csv
 import os
 import argparse
-import datetime
 import sys
 
 def check_logs():
@@ -37,43 +36,40 @@ def parse_logs():
     print("max freq %2.2f MHz" % max_freq)
     return flops, luts, max_freq
 
-def add_to_log(flops, luts, max_freq):
+def add_to_log(commit, flops, luts, max_freq):
     csvfile_created = False
     if os.path.isfile(args.csvfile):
         print("found existing csvfile %s" % args.csvfile)
         csvfile_created = True
         
     with open(args.csvfile, 'a+') as csvfile:
-        fieldnames = ['date', 'commit', 'message', 'flops', 'luts', 'freq']
+        fieldnames = ['commit', 'flops', 'luts', 'freq']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         if not csvfile_created:
             print("writing header")
             writer.writeheader()
 
-        writer.writerow({
-                            'date'      : datetime.date.today(),
-                            'commit'    : args.commit,
-                            'message'   : args.message,
+        writer.writerow({   'commit'    : commit.hexsha,
                             'flops'     : flops,
                             'luts'      : luts,
                             'freq'      : max_freq })
 
 def load_csv():
     with open(args.csvfile) as csvfile:
-        fieldnames = ['date', 'commit', 'message', 'flops', 'luts', 'freq']
+        fieldnames = ['commit', 'flops', 'luts', 'freq']
         reader = csv.DictReader(csvfile, fieldnames=fieldnames)
         return list(reader)[1:] # skip header
-
 
 def plot(data):
     import matplotlib.pyplot as plt
     import matplotlib
-    import datetime as dt
 
     fig, ax = plt.subplots()
 
-    date =  [dt.datetime.strptime(d['date'],'%Y-%m-%d').date() for d in data]
+    repo = git.Repo(args.git)
+    date =  [repo.commit(d['commit']).committed_datetime for d in data]
+    mesg =  [repo.commit(d['commit']).message for d in data]
     flops = [int(d['flops'])  for d in data]
     luts =  [int(d['luts'])   for d in data]
     freq =  [float(d['freq']) for d in data]
@@ -97,9 +93,9 @@ def plot(data):
     def hover(event):
         if event.inaxes == ax2:
             for index, d in enumerate(date):
-                if(matplotlib.dates.num2date(event.xdata).date() < d):
+                if(matplotlib.dates.num2date(event.xdata) < d):
                     break
-            new_text = "%s: %s" % (data[index]['commit'], data[index]['message'])
+            new_text = "%s: %s" % (data[index]['commit'], mesg[index])
             if commit_details.get_text() != new_text:
                 commit_details.set_text(new_text)
                 fig.canvas.draw_idle()
@@ -110,20 +106,24 @@ def plot(data):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="logLUTs - parse yosys and nextpnr logs to log FPGA resource usage")
+    parser.add_argument('--csvfile', action='store', default="LUTs.csv")
     parser.add_argument('--yosys-log', action='store', default="yosys.log")
     parser.add_argument('--nextpnr-log', action='store', default="nextpnr.log")
-    parser.add_argument('--csvfile', action='store', default="LUTs.csv")
-    parser.add_argument('--message', action='store', default="no message")
-    parser.add_argument('--commit', action='store', default="no commit")
-    parser.add_argument('--store', action='store_const', const=True)
+    parser.add_argument('--add-commit', action='store_const', const=True, help='add latest commit to the log')
+    parser.add_argument('--git', action='store', default=".git", help='where the git repo is')
     parser.add_argument('--plot', action='store_const', const=True)
     args = parser.parse_args()
 
-    check_logs()
+    try:
+        repo = git.Repo(args.git)
+    except git.exc.NoSuchPathError:
+        sys.exit("couldn't find git repo")
 
-    if args.store:
+    if args.add_commit:
+        check_logs()
         flops, luts, max_freq = parse_logs()
-        add_to_log(flops, luts, max_freq)
+        commit = repo.commit('master')
+        add_to_log(commit, flops, luts, max_freq)
 
     if args.plot:
         plot(load_csv()) 
